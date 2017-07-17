32f21756ca646dcb7da75bb8f9bf3775415b913a
Ivan Tse <ivan.tse1@gmail.com>
2017-07-17 17:31:15 -0400

    Some more functionality

--- goto_with_common_suffix.py
+++ goto_with_common_suffix.py
@@ -10,37 +10,55 @@ class GotoWithCommonSuffixCommand(sublime_plugin.WindowCommand):
     def on_done(self, text):
         original_text = text
         text = text.strip()
-        parse_result = urlparse(text).path
-        text = text if not parse_result else parse_result
-        text = text[1:] if text[0] == os.sep else text
+        line_number = None
+
+        # Attempt to parse uri-like paths
+        parse_result = urlparse(text)
+        if parse_result:
+            text = parse_result.path
+            if parse_result.fragment and parse_result.fragment[1:]:
+                # GitHub urls look like https://github.com/owner/repo/blob/master/lib/Example/Class.php#L714-L721
+                line_number = re.sub(r'-L\d+', '', parse_result.fragment[1:])
+
+        # Match line number format Class.php:220
         basename = os.path.basename(text)
+        matches = re.match(r'(?P<basename>[^:]*):(?P<line_number>\d+)', basename)
+        if matches:
+            line_number = matches.group('line_number')
+            basename = matches.group('basename')
+
+        folders = list(filter(None, text.split(os.sep)))
+
+        # Loop through each opened directory in the sublime window and open first match
         for path in self.window.folders():
+            folders_to_match = list(folders)
             prefix_path = path
-            folders = text.split(os.sep)
-            while len(folders) > 0:
+            while len(folders_to_match) > 0:
                 matched = False
+                current_folder = folders_to_match[0]
                 for directory in os.listdir(path):
-                    if os.path.isdir(os.path.join(path, directory)) and folders[0] == directory:
-                        path = path + os.sep + folders[0]
-                        folders = folders[1:]
+                    if os.path.isdir(os.path.join(path, directory)) and current_folder == directory:
+                        path = os.path.join(path, current_folder)
+                        folders_to_match = self.__pop_if(folders_to_match)
                         matched = True
                         break
-                folders = folders[1:] if not matched else folders
+                folders_to_match = self.__pop_if(folders_to_match, not matched)
 
-            if os.path.exists(os.path.join(prefix_path, basename)):
-                path = os.path.join(prefix_path, basename)
-
-            if path != '' and path != prefix_path:
-                matched_file = os.path.exists(path + os.sep + basename)
-                path = path.replace(prefix_path, '')
-                path = path[1:] if path[0] == os.sep else path
-                path = path + os.sep + basename if matched_file else path
-
-
+            absolute_path = os.path.join(path, basename)
+            if os.path.exists(absolute_path):
+                absolute_path = absolute_path + ":" + line_number if line_number else absolute_path
+                self.window.open_file(absolute_path, sublime.ENCODED_POSITION);
+                return
+            elif path != prefix_path:
+                relative_path = path.replace(prefix_path, '')
+                relative_path = self.__pop_if(relative_path, relative_path[0] == os.sep)
                 self.window.run_command("show_overlay", {
                     "overlay": "goto",
-                    "text": path
+                    "text": relative_path
                 })
                 return
         self.window.run_command("show_overlay", {"overlay": "goto", "text": original_text})
         self.window.status_message("Sorry, there was no common suffix match")
+
+    def __pop_if(self, ary, cond = True):
+        return ary[1:] if cond else ary
